ae28337ba3d25f6da12c3bcbb417ff0678a5d797
Matt Grey <matthewgrey_04@msn.com>
2017-12-20 11:49:24 +0000

    #24 search - display a message when theres no results

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+---
+# Dashes allow Jekyll goodness
+---
+
 var options = {
     valueNames: [ 'title', 'author' ]
 };
 
-var hackerList = new List('search-list', options);
+var examplesList = new List('search-list', options);
+var noItems = '<div class="tile" id="#resultNotFoundTile">' +
+                '<a href="{{site.contribute-url}}" class="full-link">' +
+                  '<h3 class="h2 title" style="margin-top: 0; margin-bottom: 0;">Nothing found</h3>' +
+                '</a>'
+              '</div>';
+
+var ghost = $('#contributeTile');
+
+examplesList.on('updated', function(list) {
+  if (list.matchingItems.length == 0) {
+    $(list.list).append(noItems);
+    $(list.list).append(ghost);
+
+  } else {
+    $('#resultNotFoundTile').detach();
+  }
+});
