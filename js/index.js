8180a2bc692fafab5c8e17557bcb154403b11c5a
Ben Brignell <benbrignell@gmail.com>
2018-01-05 16:34:15 +0000

    added sorting arrow and transitions - fixed indenting index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ var options = {
 var userList = new List('title', options);
 
 var examplesList = new List('search-list', options);
-var noItems =  '<div class="tile" id="#resultNotFoundTile">' +
+var noItems = '<div class="tile" id="#resultNotFoundTile">' +
                 '<a href="{{site.contribute-url}}" class="full-link">' +
                   '<h3 class="h2 title" style="margin-top: 0; margin-bottom: 0;">Nothing found</h3>' +
                 '</a>' +
@@ -17,7 +17,7 @@ var noItems =  '<div class="tile" id="#resultNotFoundTile">' +
                 '<h2 class="h3 lhs"><a href="{{site.contribute-url}}">Got something <br>to add?</a></h2>' +
                 '<p><a href="{{site.contribute-url}}">Submit an example</a></p> ' +
                 '</a>' +
-                '</div>';
+              '</div>';
 
 var ghost = $('#contributeTile');
 
