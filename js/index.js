ea22e13d79d54916d1998e1a9c2b319c2842e2ec
Ben Brignell <benbrignell@gmail.com>
2018-01-05 15:48:14 +0000

    removed ghost tile from main search list added ghost to no search results js

--- js/index.js
+++ js/index.js
@@ -8,11 +8,16 @@ var options = {
 var userList = new List('title', options);
 
 var examplesList = new List('search-list', options);
-var noItems = '<div class="tile" id="#resultNotFoundTile">' +
+var noItems =  '<div class="tile" id="#resultNotFoundTile">' +
                 '<a href="{{site.contribute-url}}" class="full-link">' +
                   '<h3 class="h2 title" style="margin-top: 0; margin-bottom: 0;">Nothing found</h3>' +
-                '</a>'
-              '</div>';
+                '</a>' +
+              '</div>' +
+              '<div class="tile ghost" id="contributeTile">' +
+                '<h2 class="h3 lhs"><a href="{{site.contribute-url}}">Got something <br>to add?</a></h2>' +
+                '<p><a href="{{site.contribute-url}}">Submit an example</a></p> ' +
+                '</a>' +
+                '</div>';
 
 var ghost = $('#contributeTile');
 
