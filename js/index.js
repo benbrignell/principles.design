565704b6eb9e20a02ccaaf5d75e098559d97cf81
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-20 21:14:39 +0200

    Removes jQuery dependency

--- js/index.js
+++ js/index.js
@@ -5,28 +5,33 @@
 var options = {
     valueNames: [ 'title', 'author' ]
 };
+
 var userList = new List('title', 'author', options);
-
 var examplesList = new List('search-list', options);
-var noItems = '<div class="tile" id="#resultNotFoundTile">' +
-                '<a href="{{site.contribute-url}}" class="full-link">' +
-                  '<h3 class="h2 title" style="margin-top: 0; margin-bottom: 0;">Nothing found</h3>' +
-                '</a>' +
-              '</div>' +
-              '<div class="tile ghost" id="contributeTile">' +
-                '<h2 class="h3 lhs"><a href="{{site.contribute-url}}">Got something <br>to add?</a></h2>' +
-                '<p><a href="{{site.contribute-url}}">Submit an example</a></p> ' +
-                '</a>' +
-              '</div>';
 
-var ghost = $('#contributeTile');
+var notFound = document.createElement('div');
+notFound.id = 'resultNotFoundTile'
+notFound.className = 'tile'
+notFound.innerHTML = '<a href="{{site.contribute-url}}" class="full-link">' +
+                       '<h3 class="h2 title" style="margin-top: 0; margin-bottom: 0;">Nothing found</h3>' +
+                     '</a>';
+
+var contribute = document.createElement('div');
+contribute.id = 'contributeTile'
+contribute.className = 'tile ghost'
+contribute.innerHTML = '<h2 class="h3 lhs"><a href="{{site.contribute-url}}">Got something <br>to add?</a></h2>' +
+                       '<p><a href="{{site.contribute-url}}">Submit an example</a></p>';
 
 examplesList.on('updated', function(list) {
   if (list.matchingItems.length == 0) {
-    $(list.list).append(noItems);
-    $(list.list).append(ghost);
-
+    list.list.appendChild(notFound);
+    list.list.appendChild(contribute);
   } else {
-    $('#resultNotFoundTile').detach();
+    var notFoundElem = document.getElementById('resultNotFoundTile');
+    var contributeElem = document.getElementById('contributeTile');
+    if (notFoundElem && contributeElem) {
+      notFoundElem.parentNode.removeChild(notFoundElem);
+      contributeElem.parentNode.removeChild(contributeElem);
+    }
   }
 });
